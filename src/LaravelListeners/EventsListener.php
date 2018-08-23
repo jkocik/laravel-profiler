f80313048ac8d9b70f7af7bfa3b464d8f3ab17bd
Janusz Kocik <janusz.kocik@crm.media.pl>
2018-08-23 18:33:42 -0400

    Optimize events tracker

--- src/LaravelListeners/EventsListener.php
+++ src/LaravelListeners/EventsListener.php
@@ -4,7 +4,6 @@ namespace JKocik\Laravel\Profiler\LaravelListeners;
 
 use Illuminate\Events\Dispatcher;
 use Illuminate\Support\Collection;
-use JKocik\Laravel\Profiler\Services\ParamsService;
 use JKocik\Laravel\Profiler\Contracts\LaravelListener;
 
 class EventsListener implements LaravelListener
@@ -15,25 +14,27 @@ class EventsListener implements LaravelListener
     protected $dispatcher;
 
     /**
-     * @var ParamsService
+     * @var array
      */
-    protected $paramsService;
+    protected $event = [];
 
     /**
-     * @var Collection
+     * @var array
      */
-    protected $events;
+    protected $payload = [];
+
+    /**
+     * @var array
+     */
+    protected $name = [];
 
     /**
      * EventsListener constructor.
      * @param Dispatcher $dispatcher
-     * @param ParamsService $paramsService
      */
-    public function __construct(Dispatcher $dispatcher, ParamsService $paramsService)
+    public function __construct(Dispatcher $dispatcher)
     {
         $this->dispatcher = $dispatcher;
-        $this->paramsService = $paramsService;
-        $this->events = new Collection();
     }
 
     /**
@@ -42,13 +43,9 @@ class EventsListener implements LaravelListener
     public function listen(): void
     {
         $this->dispatcher->listen('*', function ($event, $payload = null) {
-            $name = $this->resolveName($event, $payload);
-            $data = $this->resolveData($event, $payload);
-
-            $this->events->push([
-                'name' => $name,
-                'data' => $data,
-            ]);
+            $this->event[] = $event;
+            $this->payload[] = $payload;
+            $this->name[] = $this->resolveName($event, $payload);
         });
     }
 
@@ -57,7 +54,7 @@ class EventsListener implements LaravelListener
      */
     public function events(): Collection
     {
-        return $this->events;
+        return Collection::make($this->event)->zip($this->payload, $this->name);
     }
 
     /**
@@ -68,62 +65,5 @@ class EventsListener implements LaravelListener
     protected function resolveName($event, $payload): string
     {
         return is_array($payload) ? $event : $this->dispatcher->firing();
-    }
-
-    /**
-     * @param $event
-     * @param $payload
-     * @return Collection
-     */
-    protected function resolveData($event, $payload): Collection
-    {
-        return $this->data(is_array($payload) ? $payload[0] : $event);
-    }
-
-    /**
-     * @param $event
-     * @return Collection
-     */
-    protected function data($event): Collection
-    {
-        $class = new \ReflectionClass($event);
-
-        $publicProps = Collection::make(
-            $class->getProperties(\ReflectionProperty::IS_PUBLIC)
-        );
-
-        if (method_exists($publicProps, 'mapWithKeys')) {
-            return $this->propsMapWithKeys($publicProps, $event);
-        }
-
-        return $this->propsMap($publicProps, $event);
-    }
-
-    /**
-     * @param Collection $publicProps
-     * @param $event
-     * @return Collection
-     */
-    protected function propsMapWithKeys(Collection $publicProps, $event): Collection
-    {
-        return $publicProps->mapWithKeys(function ($prop) use ($event) {
-            return [
-                $prop->name => $this->paramsService->resolve($event->{$prop->name}),
-            ];
-        });
-    }
-
-    /**
-     * @param Collection $publicProps
-     * @param $event
-     * @return Collection
-     */
-    protected function propsMap(Collection $publicProps, $event): Collection
-    {
-        return $publicProps->map(function ($prop) use ($event) {
-            return [
-                $prop->name => $this->paramsService->resolve($event->{$prop->name}),
-            ];
-        })->flatten(1);
     }
 }
--- src/Trackers/EventsTracker.php
+++ src/Trackers/EventsTracker.php
@@ -2,11 +2,18 @@
 
 namespace JKocik\Laravel\Profiler\Trackers;
 
+use Illuminate\Support\Collection;
 use Illuminate\Foundation\Application;
+use JKocik\Laravel\Profiler\Services\ParamsService;
 use JKocik\Laravel\Profiler\LaravelListeners\EventsListener;
 
 class EventsTracker extends BaseTracker
 {
+    /**
+     * @var ParamsService
+     */
+    protected $paramsService;
+
     /**
      * @var EventsListener
      */
@@ -15,12 +22,14 @@ class EventsTracker extends BaseTracker
     /**
      * EventsTracker constructor.
      * @param Application $app
+     * @param ParamsService $paramsService
      * @param EventsListener $eventsListener
      */
-    public function __construct(Application $app, EventsListener $eventsListener)
+    public function __construct(Application $app, ParamsService $paramsService, EventsListener $eventsListener)
     {
         parent::__construct($app);
 
+        $this->paramsService = $paramsService;
         $this->eventsListener = $eventsListener;
         $this->eventsListener->listen();
     }
@@ -30,8 +39,65 @@ class EventsTracker extends BaseTracker
      */
     public function terminate(): void
     {
-        $events = $this->eventsListener->events()->values();
+        $events = $this->eventsListener->events()->map(function ($item) {
+            $data = $this->resolveData($item[0], $item[1]);
+
+            return [
+                'name' => $item[2],
+                'data' => $data,
+            ];
+        });
 
         $this->data->put('events', $events);
+    }
+
+    /**
+     * @param $event
+     * @param $payload
+     * @return Collection
+     */
+    protected function resolveData($event, $payload): Collection
+    {
+        $event = is_array($payload) ? $payload[0] : $event;
+
+        $class = new \ReflectionClass($event);
+
+        $publicProps = Collection::make(
+            $class->getProperties(\ReflectionProperty::IS_PUBLIC)
+        );
+
+        if (method_exists($publicProps, 'mapWithKeys')) {
+            return $this->propsMapWithKeys($publicProps, $event);
+        }
+
+        return $this->propsMap($publicProps, $event);
+    }
+
+    /**
+     * @param Collection $publicProps
+     * @param $event
+     * @return Collection
+     */
+    protected function propsMapWithKeys(Collection $publicProps, $event): Collection
+    {
+        return $publicProps->mapWithKeys(function ($prop) use ($event) {
+            return [
+                $prop->name => $this->paramsService->resolve($event->{$prop->name}),
+            ];
+        });
+    }
+
+    /**
+     * @param Collection $publicProps
+     * @param $event
+     * @return Collection
+     */
+    protected function propsMap(Collection $publicProps, $event): Collection
+    {
+        return $publicProps->map(function ($prop) use ($event) {
+            return [
+                $prop->name => $this->paramsService->resolve($event->{$prop->name}),
+            ];
+        })->flatten(1);
     }
 }
