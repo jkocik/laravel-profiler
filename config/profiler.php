5f6eb97ef777596bdf42bf45038a0786cb32b27d
Janusz Kocik <janusz.kocik@crm.media.pl>
2018-09-04 15:14:40 -0400

    Add comments to configuration file

--- config/profiler.php
+++ config/profiler.php
@@ -1,12 +1,45 @@
 <?php
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | General profiler enable / disable setting
+    |--------------------------------------------------------------------------
+    |
+    | This value enables / disables profiler. If set to false all settings
+    | below it do not take effect.
+    |
+    */
+
     'enabled' => env('PROFILER_ENABLED', true),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Override enabled profiler by env
+    |--------------------------------------------------------------------------
+    |
+    | Can disable profiler on particular env. Profiler is disabled by default
+    | on production. Take effect only if above (enabled) is set to true.
+    |
+    */
+
     'force_disable_on' => [
         'production' => true,
-        'testing' => false,
-        'local' => false,
+//        'testing' => true,
+//        'local' => true,
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Profiler trackers
+    |--------------------------------------------------------------------------
+    |
+    | Trackers collect data of framework execution. You can decide what data
+    | you want to collect by commenting / uncommenting particular trackers.
+    |
+    */
+
     'trackers' => [
         \JKocik\Laravel\Profiler\Trackers\ConfigTracker::class, // App > Config tab
         \JKocik\Laravel\Profiler\Trackers\ServiceProvidersTracker::class, // App > Service Providers tab
@@ -20,19 +53,64 @@ return [
         \JKocik\Laravel\Profiler\Trackers\EventsTracker::class, // Events tab
         \JKocik\Laravel\Profiler\Trackers\QueriesTracker::class, // Queries tab
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Profiler processors
+    |--------------------------------------------------------------------------
+    |
+    | Processors process data collected by trackers. Default processor
+    | broadcasts data through profiler server to profiler client.
+    |
+    */
+
     'processors' => [
         \JKocik\Laravel\Profiler\Processors\BroadcastingProcessor::class,
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | BroadcastingProcessor settings
+    |--------------------------------------------------------------------------
+    |
+    | These settings let you set up connection between profiler and profiler
+    | server using http protocol.
+    |
+    */
+
     'broadcasting' => [
         'address' => 'http://localhost',
         'port' => '8099',
         'log_errors_enabled' => true,
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Data passed to tracked items
+    |--------------------------------------------------------------------------
+    |
+    | That can be very heavy when your views or events receive a lot of data.
+    | Avoid using it on testing env specially when running whole test suite.
+    |
+    */
+
     'data' => [
         'views' => false,
         'events' => false,
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Grouping
+    |--------------------------------------------------------------------------
+    |
+    | There can be a lot of the same events fired one by one. Grouping can
+    | help you review them on better organized list of events.
+    |
+    */
+
     'group' => [
         'events' => true,
     ],
+
 ];
