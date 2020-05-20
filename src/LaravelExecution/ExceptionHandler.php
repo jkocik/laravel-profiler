248dd12cf4f212a96da9024a059b371e358f12d4
Ulrich Thomas Gabor <ulrich.gabor@tu-dortmund.de>
2020-05-20 16:40:20 +0200

    Fix for laravel 7

--- src/LaravelExecution/ExceptionHandler.php
+++ src/LaravelExecution/ExceptionHandler.php
@@ -2,17 +2,16 @@
 
 namespace JKocik\Laravel\Profiler\LaravelExecution;
 
-use Exception;
 use App\Exceptions\Handler;
 use JKocik\Laravel\Profiler\Events\ExceptionHandling;
 
 class ExceptionHandler extends Handler
 {
     /**
-     * @param Exception $exception
+     * @param \Throwable $exception
      * @return void
      */
-    public function report(Exception $exception)
+    public function report(\Throwable $exception)
     {
         event(new ExceptionHandling($exception));
 
